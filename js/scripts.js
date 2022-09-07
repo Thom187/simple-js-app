9de82b9b3197ae465dc9b04860a3b200a4d9f3db
Thom187 <tom.hoppe@gmx.de>
2022-09-07 15:04:47 +0200

    Update scripts.js
    Defined the variable pokemonList, storing a array of objects that store pokemon data

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// Creating a [array] of {objects} using 'strings' and floats --> e.g. 0.7
+//              complex data               primitive data
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    weight: 6.9,
+    type: [ 'grass', 'poison'],
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    weight: 8.5,
+    type: [ 'fire', 'fire'],
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    weight: 9.0,
+    type: [ 'water', 'water']
+  },
+  {
+    name: 'Caterpie',
+    height: 0.3,
+    weight: 2.9,
+    type: [ 'bug', 'bug']
+  },
+  {
+    name: 'Pidgey',
+    height: 0.3,
+    weight: 1.8,
+    type: [ 'flying', 'normal']
+  }
+];

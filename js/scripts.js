9839a7daa9140403ef18d103585907befb1e7952
Thom187 <tom.hoppe@gmx.de>
2022-09-08 17:05:42 +0200

    Update scripts.js
    Added a conditional to highlight the largest Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -33,8 +33,13 @@ let pokemonList = [
   }
 ];
 
-// Create a Loop that iterates over each Item of the array and prints its name and height
+// Create a "for" - Loop that iterates over each Item of the array and prints its name and height
+// Use a conditional/ternary operator (if) within the loop to print a comment for the largest Pokemon
 
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '</p>')
+  if (pokemonList[i].height > 0.6) {
+      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + ' Wow, that\'s big!' + '</p>');
+    } else {
+      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '</p>');
+    }
 }

fc788fe62f7bb5a139d9a737555ac73a230eaff1
Thom187 <tom.hoppe@gmx.de>
2022-09-08 17:26:43 +0200

    Update scripts.js
    Improved indentation

--- js/scripts.js
+++ js/scripts.js
@@ -5,41 +5,56 @@ let pokemonList = [
     name: 'Bulbasaur',
     height: 0.7,
     weight: 6.9,
-    type: [ 'grass', 'poison'],
+    type: ['grass', 'poison'],
   },
   {
     name: 'Charmander',
     height: 0.6,
     weight: 8.5,
-    type: [ 'fire', 'fire'],
+    type: ['fire', 'fire'],
   },
   {
     name: 'Squirtle',
     height: 0.5,
     weight: 9.0,
-    type: [ 'water', 'water']
+    type: ['water', 'water'],
   },
   {
     name: 'Caterpie',
     height: 0.3,
     weight: 2.9,
-    type: [ 'bug', 'bug']
+    type: ['bug', 'bug'],
   },
   {
     name: 'Pidgey',
     height: 0.3,
     weight: 1.8,
-    type: [ 'flying', 'normal']
-  }
+    type: ['flying', 'normal'],
+  },
 ];
 
-// Create a "for" - Loop that iterates over each Item of the array and prints its name and height
+// Create a 'for' - Loop that iterates over each Item of the array and prints its name and height
 // Use a conditional/ternary operator (if) within the loop to print a comment for the largest Pokemon
 
 for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height > 0.6) {
-      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + ' Wow, that\'s big!' + '</p>');
-    } else {
-      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '</p>');
-    }
+    document.write(
+      '<p>' +
+        pokemonList[i].name +
+        ' (height: ' +
+        pokemonList[i].height +
+        ') ' +
+        ' Wow, that\'s big!' +
+        '</p>'
+    );
+  } else {
+    document.write(
+      '<p>' +
+        pokemonList[i].name +
+        ' (height: ' +
+        pokemonList[i].height +
+        ') ' +
+        '</p>'
+    );
+  }
 }
